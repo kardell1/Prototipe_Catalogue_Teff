bdfde25ce80f685cd4a1033cb9d7a082a7365e19
kardell1 <kevindavisilva97@gmail.com>
2024-05-16 20:20:06 -0400

    pr

--- src/components/Product/Description.tsx
+++ src/components/Product/Description.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Modal } from "./Modal";
 // <!--
 //     nombre del objeto
 //     titulo
@@ -22,24 +23,12 @@ export const Description = () => {
   // console.log("los datos recibidos son : " + price )
 
   const [modal, setModal] = useState(false);
-  const [amount, setAmount] = useState(1);
-  const [prices, setPrice] = useState(price);
-  const handleAmount = (event: string) => {
-    if (amount === 0 && event === "less") {
-      setAmount(0);
-      setPrice(0);
-    } else if (event === "most") {
-      setAmount(amount + 1);
-      setPrice(prices + price);
-    } else if (event === "less") {
-      setAmount(amount - 1);
-      setPrice(prices - price);
-    }
-  };
+  
   const handleModal = (bool: boolean) => {
     setModal(bool);
     console.log("abriendo modal");
   };
+
 
   return (
     <div className="bg-stone-900 text-slate-200 p-10 flex flex-col gap-4 relative ">
@@ -58,78 +47,7 @@ export const Description = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam vel quasi
       </p>
       {modal ? (
-        <section className="absolute rounded-2xl bg-slate-100 text-slate-600 p-5 min-w-[80%]   ">
-          <div className="flex flex-col ">
-            <div className="flex justify-between gap-5">
-              <p className="text-3xl font-semibold">Orden</p>
-              <button
-                className="bg-red-500 rounded-full px-3"
-                onClick={() => handleModal(false)}
-              >
-                X
-              </button>
-            </div>
-            <div className="p-4  ">
-              <p className="font-semibold "> Datos personales </p>
-              <div className="flex flex-col  ">
-                <input
-                  className="bg-slate-100 p-2 outline-none border-b-slate-300 border-b-2 "
-                  placeholder="Nombre"
-                  type="text"
-                />
-              </div>
-            </div>
-            <div className="p-4">
-              <p className="font-semibold"> Datos contacto </p>
-              <div className="flex flex-col gap-5">
-                <input
-                  className="bg-slate-100 p-2 outline-none border-b-slate-300 border-b-2 "
-                  placeholder="Celular"
-                  type="text"
-                />
-                <input
-                  className="bg-slate-100 p-2 outline-none border-b-slate-300 border-b-2 "
-                  placeholder="Correo electronico"
-                  type="text"
-                />
-              </div>
-            </div>
-            <div className="flex justify-between  uppercase text-sm font-medium py-2">
-              <div className="">
-                <p>amount</p>
-                <div className="flex justify-between items-center bg-slate-700 text-slate-100 py-1 px-2 outline-none border-b-slate-300 border-b-2 w-20 rounded-2xl  ">
-                  <button
-                    className="hover:text-green-600 text-xl "
-                    onClick={() => {
-                      handleAmount("less");
-                    }}
-                  >
-                    -
-                  </button>
-                  <p className="text-base"> {amount} </p>
-                  <button
-                    className="hover:text-green-600 text-xl"
-                    onClick={() => {
-                      handleAmount("most");
-                    }}
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
-              <div>
-                <p>price</p>
-                <p className="text-green-600 font-bold text-base ">
-                  {" "}
-                  {prices}{" "}
-                </p>
-              </div>
-            </div>
-            <button className="bg-stone-900 text-slate-100 p-2 font-bold rounded-2xl ">
-              BUY
-            </button>
-          </div>
-        </section>
+        <Modal price={price} handleModal = {handleModal} />
       ) : (
         ""
       )}
--- src/components/Product/Modal.tsx
+++ src/components/Product/Modal.tsx
@@ -1,7 +1,137 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export const Modal = () => {
+type Tmodal = {
+  price: number;
+  handleModal: (bool: boolean) => void;
+};
+
+export const Modal = ({ price, handleModal }: Tmodal) => {
+  const [amount, setAmount] = useState(1);
+  const [prices, setPrice] = useState(price);
+  const handleAmount = (event: string) => {
+    if (amount === 0 && event === "less") {
+      setAmount(0);
+      setPrice(0);
+    } else if (event === "most") {
+      setAmount(amount + 1);
+      setPrice(prices + price);
+    } else if (event === "less") {
+      setAmount(amount - 1);
+      setPrice(prices - price);
+    }
+  };
+  const [info, setInfo] = useState({
+    name: "",
+    cellphone: "",
+    email: "",
+  });
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setInfo((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("pulsado el formulario");
+
+    console.log("datos del formulario son : " + info);
+
+    console.log("datos del formulario son : " + JSON.stringify(info) );
+
+    console.log("y el precio fijado es : " + prices );
+
+
+    console.log("la cantidad fijada es  : " + amount);
+  };
   return (
-    <div>Modal</div>
-  )
-}
+    <section className="absolute rounded-2xl bg-slate-100 text-slate-600 p-5 min-w-[80%]   ">
+      <form onSubmit={handleForm}>
+        <div className="flex flex-col ">
+          <div className="flex justify-between gap-5">
+            <p className="text-3xl font-semibold">Orden</p>
+            <button
+              type="button"
+              className="bg-red-500 rounded-full px-3"
+              onClick={() => handleModal(false)}
+            >
+              X
+            </button>
+          </div>
+          <div className="p-4  ">
+            <p className="font-semibold "> Datos personales </p>
+            <div className="flex flex-col  ">
+              <input
+                className="bg-slate-100 p-2 outline-none border-b-slate-300 border-b-2 "
+                placeholder="Nombre"
+                type="text"
+                name="name"
+                onChange={handleInput}
+                value={info.name}
+              />
+            </div>
+          </div>
+          <div className="p-4">
+            <p className="font-semibold"> Datos contacto </p>
+            <div className="flex flex-col gap-5">
+              <input
+                className="bg-slate-100 p-2 outline-none border-b-slate-300 border-b-2 "
+                placeholder="Celular"
+                type="text"
+                name="cellphone"
+                onChange={handleInput}
+                value={info.cellphone}
+              />
+              <input
+                className="bg-slate-100 p-2 outline-none border-b-slate-300 border-b-2 "
+                placeholder="Correo electronico"
+                type="email"
+                name="email"
+                onChange={handleInput}
+                value={info.email}
+              />
+            </div>
+          </div>
+          <div className="flex justify-between  uppercase text-sm font-medium py-2">
+            <div className="">
+              <p>amount</p>
+              <div className="flex justify-between items-center bg-slate-700 text-slate-100 py-1 px-2 outline-none border-b-slate-300 border-b-2 w-20 rounded-2xl  ">
+                <button
+                  type="button"
+                  className="hover:text-green-600 text-xl "
+                  onClick={() => {
+                    handleAmount("less");
+                  }}
+                >
+                  -
+                </button>
+                <p className="text-base"> {amount} </p>
+                <button
+                  type="button"
+                  className="hover:text-green-600 text-xl"
+                  onClick={() => {
+                    handleAmount("most");
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div>
+              <p>price</p>
+              <p className="text-green-600 font-bold text-base "> {prices} </p>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-stone-900 text-slate-100 p-2 font-bold rounded-2xl "
+          >
+            BUY
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
